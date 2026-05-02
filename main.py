09d9ebe0d70b5f786c8a912768f29b10d08ecaf8
CoolCoder707 <dirggaming303@gmail.com>
2026-05-02 12:15:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
 import os
-import requests
 
-BOT_TOKEN = os.getenv("8790935199:AAHTjA6v2G4FHVmbgb-EnkxHLgTIyivZ1Kg")
-CHAT_ID = os.getenv("7171044211")
-
-if not BOT_TOKEN or not CHAT_ID:
-    raise Exception("❌ BOT_TOKEN or CHAT_ID missing")
-
-def send(msg):
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    requests.post(url, data={"chat_id": CHAT_ID, "text": msg})
-
-def main():
-    send("✅ Bot is LIVE and working!")
-
-if __name__ == "__main__":
-    main()
+print("BOT_TOKEN:", os.getenv("BOT_TOKEN"))
+print("CHAT_ID:", os.getenv("CHAT_ID"))
